821b02e5ca176170886a0da8f91d30e84ba78c75
chikamura <mchikamura01@gmail.com>
2024-12-01 20:06:01 +0900

    add key store and trust store descriptions to readme.md

--- src/test/resources/initdb.d/init.js
+++ src/test/resources/initdb.d/init.js
@@ -2,8 +2,9 @@ db.getSiblingDB("$external").runCommand(
   {
     createUser: "CN=localhost",
     roles: [
-         { role: "readWrite", db: "mydb" },
-         { role: "userAdminAnyDatabase", db: "admin" }
+      { role: "readWrite", db: "mydb" },
+      { role: "readWrite", db: "embulk_test" },
+      { role: "userAdminAnyDatabase", db: "admin" }
     ],
     writeConcern: { w: "majority" , wtimeout: 5000 }
   }
@@ -11,10 +12,22 @@ db.getSiblingDB("$external").runCommand(
 
 db.getSiblingDB("mydb").runCommand(
   {
-    createUser:'mongo_user',
+    createUser: 'mongo_user',
     pwd:'dbpass',
     roles:[
-      {role:'readWrite',db:'mydb'}
+      { role:'readWrite', db:'mydb' }
+    ]
+  }
+)
+
+db.getSiblingDB("embulk_test").runCommand(
+  {
+    createUser: 'mongo_user',
+    pwd:'dbpass',
+    roles:[
+      { role:'readWrite', db:'embulk_test' }
     ]
   }
 );
+
+db.getSiblingDB("embulk_test").products.insert({category : 'A' , name : "camera case" , stock : 5});
